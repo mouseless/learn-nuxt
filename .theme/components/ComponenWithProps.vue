5ef8d88ce5337f459c18d2b5d0009ca4a23a6ab4
alptuncs <alptuncsever@gmail.com>
2023-02-23 15:25:23 +0300

    add component with props & demo

---
+++ .theme/components/ComponenWithProps.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {data}
+  </div>
+</template>
+
+<script> 
+const props = defineProps({
+  data: {
+    type: String,
+    default: null
+  }
+});
+</script>
---
+++ .theme/pages/demo/components/components-with-props.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <ComponentWithProps data="Data" />
+    <ComponentWithProps data="DifferentData" />
+  </div>
+</template>
