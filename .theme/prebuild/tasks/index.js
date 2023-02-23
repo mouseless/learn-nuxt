6a10a838724521f49c18981b5a69fdd5058dd0bd
cihandeniz <cihan@gcbrains.com>
2023-02-23 10:02:21 +0300

    add fix links task

--- .theme/prebuild/tasks/index.js
+++ .theme/prebuild/tasks/index.js
@@ -1,3 +1,4 @@
 export { default as clean } from "./clean.js";
 export { default as copy } from "./copy.js";
+export { default as fixLinks } from "./fixLinks.js";
 export { default as rename } from "./rename.js";
