0f8b04851478fd93e3a9b80c1d305e4b259405d3
alptuncs <alptuncsever@gmail.com>
2023-02-24 14:47:28 +0300

    create computed demo

---
+++ .theme/pages/demo/computed.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    FirstName is a property: {{ firstName }}
+    <br/>
+    LastName is a property: {{ lastName }}
+    <br/>
+    FullName is a computed property: {{ fullName }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe"
+    };
+  },
+  computed: {
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+};
+</script>
