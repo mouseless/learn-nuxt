d2f3e0eeaa787a406aa69ab93d7e1a6990b4528f
alptuncs <alptuncsever@gmail.com>
2023-03-02 13:21:43 +0300

    add prebuild to navigation

--- .theme/layouts/default.vue
+++ .theme/layouts/default.vue
@@ -3,13 +3,11 @@
     <header>
       <nav class="App__nav">
         <router-link to="/">Home</router-link> |
-        <router-link to="/contribution-guide">Contribution</router-link>
+        <router-link to="/contribution-guide">Contribution</router-link> |
+        <router-link to="/prebuild">Prebuild</router-link>
       </nav>
       <slot/>
       <router-view/>
     </header>
   </div>
 </template>
-<style>
-
-</style>
