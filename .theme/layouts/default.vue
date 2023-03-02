a2f60c2dfab28323ae34ccefc1b03e5647a0adab
Sefer Mirza <sefermirza@gmail.com>
2023-03-02 17:45:44 +0300

    fix route link & add 2 image

--- .theme/layouts/default.vue
+++ .theme/layouts/default.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <header>
+      <img
+        src="https://raw.githubusercontent.com/mouseless/brand/main/assets/logo/svg/logo-mark-primary.svg"
+        height="10"
+      > | <img
+        src="https://nuxt.com/assets/design-kit/logo/icon-green.svg"
+        height="10"
+      >
       <nav>
         <ContentQuery
           v-slot="{ data: menus }"
           path="/"
           :where="{ _dir: { $eq: '' } }"
         >
-          <NuxtLink
-            v-for="menu in menus"
-            :key="menu.path"
-            to="/"
-          >
-            {{ menu.title }}
-          </NuxtLink>
+          <ul>
+            <li
+              v-for="menu in menus"
+              :key="menu.path"
+            >
+              <NuxtLink
+                :to="menu._path"
+              >
+                | {{ menu.title }} |
+              </NuxtLink>
+            </li>
+          </ul>
         </ContentQuery>
       </nav>
     </header>
