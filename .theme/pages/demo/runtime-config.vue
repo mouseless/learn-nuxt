320a190ec4cdf08f4cca9b975ff8423e58e636d2
Sefer Mirza <sefermirza@gmail.com>
2023-03-02 12:57:34 +0300

    review

--- .theme/pages/demo/runtime-config.vue
+++ .theme/pages/demo/runtime-config.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1>Runtime Config</h1>
-    BaseUrl: {{ config.public.baseURL }}
+    <code v-pre>{{ runtimeConfig.public.baseURL }}</code> =
+    {{ runtimeConfig.public.baseURL }}
   </div>
 </template>
 
 <script setup>
 import { useRuntimeConfig } from "#app";
 
-const config = useRuntimeConfig();
+const runtimeConfig = useRuntimeConfig();
 </script>
