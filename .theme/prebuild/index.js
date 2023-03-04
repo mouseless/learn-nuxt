ae3d0d08828f344ad0b48800851a8c1f8733b33f
dncsvr <disever10@alm.ku.edu.tr>
2023-03-04 15:16:43 +0300

    prebuild submodule updates

--- .theme/prebuild/index.js
+++ .theme/prebuild/index.js
@@ -4,4 +4,8 @@ import { run } from "./.prebuild/index.js";
 
 const configPath = join(dirname(fileURLToPath(import.meta.url)), "config.yml");
 
-run(configPath);
+run(configPath, {
+  customTasks: {
+    sampleTask: async () => console.log("STARTING PREBUILD")
+  }
+});
