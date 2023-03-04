b311c1c37e2aa9fa1b678f4b720f7b9600a7bd02
dncsvr <disever10@alm.ku.edu.tr>
2023-03-04 15:24:11 +0300

    remove sampleTask

--- .theme/prebuild/index.js
+++ .theme/prebuild/index.js
@@ -4,8 +4,4 @@ import { run } from "./.prebuild/index.js";
 
 const configPath = join(dirname(fileURLToPath(import.meta.url)), "config.yml");
 
-run(configPath, {
-  customTasks: {
-    sampleTask: async () => console.log("STARTING PREBUILD")
-  }
-});
+run(configPath);
