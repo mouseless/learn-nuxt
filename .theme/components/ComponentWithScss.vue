72bdc29850feb65008695f01d613cb863f853e3e
alptuncs <alptuncsever@gmail.com>
2023-03-14 12:50:57 +0300

    add a basic component with scss

---
+++ .theme/components/ComponentWithScss.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="basic">
+    Basic component
+  </div>
+</template>
+<style lang="scss">
+.basic {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid;
+  font-size: 8rem;
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  background-color: red;
+}
+</style>
---
+++ .theme/pages/demo/scss.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <ComponentWithScss />
+  </div>
+</template>
