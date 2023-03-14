ad71ce30db8ba7acf8cbe20956db1f719a807f20
alptuncs <alptuncsever@gmail.com>
2023-03-14 18:57:07 +0300

    add mouseless font to content in global style
    add dev script without build & update run.bat and makefile

--- run.bat
+++ run.bat
@@ -1,14 +1,20 @@
 @echo off
 :BEGIN
 echo (1) Dev
-echo (2) Local
+echo (2) Dev With Build
+echo (3) Local
 CHOICE /N /C:12 /M "Pick mode"
-IF ERRORLEVEL == 2 GOTO Local
+IF ERRORLEVEL == 3 GOTO Local
+IF ERRORLEVEL == 2 GOTO Dev_With_Build
 IF ERRORLEVEL == 1 GOTO Dev
 GOTO END
 :Dev
 cd .theme
 npm run dev
+cd ..
+:Dev_With_Build
+cd .theme
+npm run dev:build
 cd ..
 :Local
 cd .theme
