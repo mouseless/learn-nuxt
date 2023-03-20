fa5dae5ca61d8fcb3ebb5ed7982aaace9df14766
alptuncs <alptuncsever@gmail.com>
2023-03-20 17:31:09 +0300

    add component and demo

---
+++ .theme/components/ComponentWithComplexType.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hello, my name is {{ person.name }} and I am {{ person.age }} years old.
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import SimpleType from "../types/SimpleType";
+
+const person = ref<SimpleType>({
+  name: "John",
+  age: 20
+});
+</script>
---
+++ .theme/pages/demo/type.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <ComponentWithComplexType />
+  </div>
+</template>
