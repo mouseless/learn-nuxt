aea52220882e0f6d61f1d77231f82bd54cbed4c5
dncsvr <disever10@alm.ku.edu.tr>
2023-03-23 20:03:46 +0300

    possible fix
    - add ProseImg
    - modify content doc

---
+++ .theme/components/Prose/ProseImg.vue
@@ -0,0 +1,39 @@
+<template>
+    <img :src="refinedSrc" :alt="alt" :width="width" :height="height"/>
+</template>
+
+<script setup>
+import { withBase } from "ufo";
+import { inject } from "vue";
+import { useRuntimeConfig, computed } from "#imports";
+
+const route = inject("route", useRuntimeConfig().app.baseURL);
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: ""
+  },
+  alt: {
+    type: String,
+    default: ""
+  },
+  width: {
+    type: [String, Number],
+    default: undefined
+  },
+  height: {
+    type: [String, Number],
+    default: undefined
+  }
+});
+const refinedSrc = computed(() => {
+  if(route !== "/" && route.toString().endsWith("/") && props.src.startsWith(".")) {
+    return props.src.substring(1);
+  }
+  if (props.src?.startsWith("/") && !props.src.startsWith("//")) {
+    return withBase(props.src, useRuntimeConfig().app.baseURL);
+  }
+  return props.src;
+});
+</script>
--- .theme/pages/[...content-page].vue
+++ .theme/pages/[...content-page].vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <ContentDoc />
+    <ContentDoc :path="docPath" />
   </div>
 </template>
+<script setup>
+// import { withTrailingSlash } from "ufo";
+import { useRoute } from "#imports";
+
+const docPath = computed(() => {
+  const x = useRoute().path;
+  return x;
+});
+</script>
