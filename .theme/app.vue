623a56053dd7cb41f356bfa2941f682cd558d521
dncsvr <disever10@alm.ku.edu.tr>
2023-03-23 14:10:46 +0300

    Update app.vue

--- .theme/app.vue
+++ .theme/app.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
     <!-- key is required to overcome hydration and multiple render issues -->
-    <NuxtPage :key="$route.path" />
+    <NuxtPage :key="$route.params.slug" />
   </NuxtLayout>
 </template>
 <script setup>
