9bc87793dd6c2ea9e06d7311c64faca8d9403a68
dncsvr <disever10@alm.ku.edu.tr>
2023-03-23 14:02:47 +0300

    remove app vue to try create trailing slash error

--- .theme/app.vue
+++ .theme/app.vue
@@ -5,19 +5,4 @@
   </NuxtLayout>
 </template>
 <script setup>
-import { useRoute, navigateTo, onMounted } from "#imports";
-
-const route = useRoute();
-
-onMounted(async () => {
-  if(route.path.endsWith("/")) {
-    const { path, query, hash } = route;
-    const nextPath = path.replace(/\/+$/, "") || "/";
-    const nextRoute = { path: nextPath, query, hash };
-
-    // works only if `router.options.strict` is enabled in `nuxt.config.ts`
-    // replace prevents browser to record this navigation in its history
-    await navigateTo(nextRoute, { replace: true });
-  }
-});
 </script>
