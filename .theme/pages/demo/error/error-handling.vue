886c0d630807f23465394e8c883a7a7a6ceeab24
Sefer Mirza <sefermirza@gmail.com>
2023-03-24 17:02:11 +0300

    create demo for createError

---
+++ .theme/pages/demo/error/error-handling.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="handleError">
+      Handle Error
+    </button>
+  </div>
+</template>
+<script setup>
+import { createError } from "#app";
+
+function handleError() {
+  throw createError({ statusCode: 404, statusMessage: "Status Message", fatal: true });
+}
+</script>
