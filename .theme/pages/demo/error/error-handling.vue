e6d45f190b99ef51873f9fa21685f3bb6b468531
Sefer Mirza <sefermirza@gmail.com>
2023-03-24 17:08:08 +0300

    rename method

--- .theme/pages/demo/error/error-handling.vue
+++ .theme/pages/demo/error/error-handling.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <button @click="handleError">
-      Handle Error
+    <button @click="throwError">
+      Throw Error
     </button>
   </div>
 </template>
 <script setup>
 import { createError } from "#app";
 
-function handleError() {
+function throwError() {
   throw createError({ statusCode: 404, statusMessage: "Status Message", fatal: true });
 }
 </script>
