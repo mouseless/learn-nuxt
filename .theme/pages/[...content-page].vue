2ac05ce5b90b2b368f7ccf6eaedb5271de50d479
dncsvr <disever10@alm.ku.edu.tr>
2023-03-24 12:33:50 +0300

    use baseUrl for href value

--- .theme/pages/[...content-page].vue
+++ .theme/pages/[...content-page].vue
@@ -4,16 +4,18 @@
   </div>
 </template>
 <script setup>
+import { useRuntimeConfig } from "#app";
 import { useRoute } from "#imports";
 
 const route = useRoute();
 const articles = await queryContent(route.path).findOne();
+const runtimeConfig = useRuntimeConfig();
 
 const dir = computed(() => {
   if(articles._dir !== "") {
-    return "/".concat(articles._dir).concat("/");
+    return runtimeConfig.public.baseURL.concat(articles._dir).concat("/");
   }
-  return "/".concat(articles._dir);
+  return runtimeConfig.public.baseURL;
 });
 
 useHead({
