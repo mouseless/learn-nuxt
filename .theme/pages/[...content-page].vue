bf2d0022a8c8ec84e5cbbfe4d7ccbde46acb20ac
dncsvr <disever10@alm.ku.edu.tr>
2023-03-24 16:04:48 +0300

    revert changes

--- .theme/pages/[...content-page].vue
+++ .theme/pages/[...content-page].vue
@@ -3,28 +3,3 @@
     <ContentDoc />
   </div>
 </template>
-<script setup>
-import { resolveURL } from "ufo";
-import { useRuntimeConfig } from "#app";
-import { useRoute } from "#imports";
-
-const route = useRoute();
-const articles = await queryContent(route.path).findOne();
-const runtimeConfig = useRuntimeConfig();
-
-const dir = computed(() => {
-  let bUrl = runtimeConfig.public.baseURL;
-  if(!bUrl.endsWith("/")) {
-    bUrl = bUrl.concat("/");
-  }
-  if(articles._dir !== "") {
-    return resolveURL(runtimeConfig.public.baseURL, articles._dir).concat("/");
-  }else{
-    return bUrl;
-  }
-});
-
-useHead({
-  base: { href: dir }
-});
-</script>
