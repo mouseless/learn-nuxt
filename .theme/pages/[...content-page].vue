9b7a946d157ec5685eb29694d36ba6e3819899f8
cihandeniz <cihan@gcbrains.com>
2023-03-25 10:32:16 +0300

    debug trailing slash in test env

--- .theme/pages/[...content-page].vue
+++ .theme/pages/[...content-page].vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <ContentDoc>
+    <ContentDoc :path="path">
       <template #not-found>
         <ContentDoc path="/not-found" />
       </template>
     </ContentDoc>
   </div>
 </template>
+<script setup>
+import { withoutTrailingSlash } from "ufo";
+import { useRoute } from "#imports";
+
+const route = useRoute();
+
+const path = withoutTrailingSlash(route.path);
+
+console.log(route.path);
+console.log(path);
+</script>
