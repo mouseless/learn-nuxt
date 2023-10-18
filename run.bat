93df02a36c63cf8ba12aca9dbd9ef7efde90f119
Sefer Mirza <sefermirza@gmail.com>
2023-10-18 15:46:42 +0300

    remove build scripts & update makefile and bat scripts

--- run.bat
+++ run.bat
@@ -1,24 +1,18 @@
 @echo off
 :BEGIN
 echo (1) Dev
-echo (2) Dev With Build
-echo (3) Local
-echo (4) Eslint
-echo (5) Eslint Fix
+echo (2) Local
+echo (3) Eslint
+echo (4) Eslint Fix
 CHOICE /N /C:12345 /M "Pick mode"
-IF ERRORLEVEL == 5 GOTO Eslint-Fix
-IF ERRORLEVEL == 4 GOTO Eslint
-IF ERRORLEVEL == 3 GOTO Local
-IF ERRORLEVEL == 2 GOTO Dev_With_Build
+IF ERRORLEVEL == 4 GOTO Eslint-Fix
+IF ERRORLEVEL == 3 GOTO Eslint
+IF ERRORLEVEL == 2 GOTO Local
 IF ERRORLEVEL == 1 GOTO Dev
 GOTO END
 :Dev
 cd .theme
 npm run dev
-cd ..
-:Dev_With_Build
-cd .theme
-npm run dev:build
 cd ..
 :Local
 cd .theme
@@ -26,7 +20,7 @@ npm run local
 cd ..
 :Eslint
 cd .theme
-npx eslint .
+npm run lint
 cd ..
 :Eslint-Fix
 cd .theme
