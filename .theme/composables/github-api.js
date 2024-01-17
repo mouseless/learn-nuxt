f482a9006ae411ddff539d795481f0fb112172e4
Sefer Mirza <sefermirza@gmail.com>
2024-01-17 13:10:42 +0300

    fix url

--- .theme/composables/github-api.js
+++ .theme/composables/github-api.js
@@ -1,13 +1,12 @@
-// import { joinURL } from "ufo";
+import { joinURL } from "ufo";
 
-// const repositoryUrl = "https://api.github.com/repos/mouseless/learn-nuxt";
+const repositoryUrl = "https://api.github.com/repos/mouseless/learn-nuxt";
 
 export const fetchContributorsStats = async() => {
   // Look here for response detail and headers =>
   // https://docs.github.com/en/rest/metrics/statistics?apiVersion=2022-11-28#get-all-contributor-commit-activity
   const result = await $fetch(
-    // joinURL(repositoryUrl, "/stats/contributors"),
-    "https://webhook.site/2ee08ea1-6d52-4a81-8dc6-e23842c8704b",
+    joinURL(repositoryUrl, "/stats/contributors"),
     {
       method: "GET",
       headers: {
