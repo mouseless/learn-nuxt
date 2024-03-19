d58bacb0cf70e66fa0f3295d1e0892beeb8425b0
Sefer Mirza <sefermirza@gmail.com>
2024-03-19 16:53:37 +0300

    inline extra

--- .theme/pages/demo/pinia.vue
+++ .theme/pages/demo/pinia.vue
@@ -5,9 +5,8 @@
 import { usePiniaStore } from "~/store/piniaStore";
 
 const store = usePiniaStore();
-const titles = ["title1", "title2", "title3"];
 
 if(store.titles.length === 0) {
-  store.setTitles(titles);
+  store.setTitles(["title1", "title2", "title3"]);
 }
 </script>
