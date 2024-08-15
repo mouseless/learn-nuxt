42cd42c86b8e83fabe39d446e7a76f95b6ac1204
dncsvr <disever10@alm.ku.edu.tr>
2024-08-15 16:56:23 +0300

    disable inline style feature

--- .theme/nuxt.config.ts
+++ .theme/nuxt.config.ts
@@ -69,6 +69,9 @@ export default defineNuxtConfig({
   experimental: {
     payloadExtraction: false
   },
+  features: {
+    inlineStyles: false
+  },
   generate: {
     routes: ["/not-found"]
   },
