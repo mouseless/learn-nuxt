9be503ca396d776d38be4161f0700b75d832999e
dncsvr <disever10@alm.ku.edu.tr>
2024-11-26 12:20:12 +0300

    add demo page for mutex

---
+++ .theme/pages/demo/mutex.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex justify-around">
+    <div class="w-1/2 m-4">
+      <span class="text-l">Counter Without Mutex</span>
+      <pre>
+{{ nonMutex }}
+      </pre>
+    </div>
+    <div class="w-1/2 m-4">
+      <span pan class="text-l">Counter With Mutex</span>
+      <pre>
+{{ withMutex }}
+      </pre>
+    </div>
+  </div>
+</template>
+<script setup>
+import { Mutex } from "async-mutex";
+const mutex = new Mutex();
+
+const nonMutex = ref("");
+const withMutex = ref("");
+let nonMutexcounter = 0;
+let withMutexCounter = 0;
+
+increaseCounter(1, 500);
+increaseCounter(2, 200);
+increaseCounter(3, 500);
+increaseCounter(4, 100);
+
+increaseCounterWithMutex(1, 500);
+increaseCounterWithMutex(2, 200);
+increaseCounterWithMutex(3, 500);
+increaseCounterWithMutex(4, 100);
+
+function increaseCounter(order, ms) {
+  nonMutex.value += `Execute: ${order}\n`;
+  setTimeout(() => {
+    nonMutexcounter++;
+    nonMutex.value += `Finish: ${order}\n`;
+    nonMutex.value += `new counter is: ${nonMutexcounter}\n`;
+  }, ms);
+}
+
+function increaseCounterWithMutex(order, ms) {
+  withMutex.value += `Execute: ${order}\n`;
+  mutex
+    .acquire()
+    .then(function(release) {
+      setTimeout(() => {
+        withMutexCounter++;
+        withMutex.value += `Finish: ${order}\n`;
+        withMutex.value += `new counter is: ${withMutexCounter}\n`;
+        release();
+      }, ms);
+    });
+}
+</script>
